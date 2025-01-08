dbcec1cbc0a5d3e1f942e408190f48ef4e4fd35d
Andrinkaech <andrin_kaech@sluz.ch>
2025-01-08 15:31:14 +0100

    tes

--- ict-m324-2024-public-task-1-npm-solution/index.js
+++ ict-m324-2024-public-task-1-npm-solution/index.js
@@ -4,7 +4,7 @@ import chalk from 'chalk';
 const word1 = generate({ exactly: 1, minLength: 10, wordsPerString: 1 })[0];
 const word2 = generate();
 
-console.log(`The ${chalk.greenBright(word1)} is ${chalk.redBright(word2)}.`);
+console.log(`Te ${chalk.greenBright(word1)} is ${chalk.redBright(word2)}.`);
 
 export const sum = (a, b) => a + b;
 
